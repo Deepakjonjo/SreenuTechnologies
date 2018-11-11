712374b5722b869ebb23a6b1bf9b8f42cfd05bfd
Deepakjonjo <31859319+Deepakjonjo@users.noreply.github.com>
2018-11-11 22:22:58 +0530

    update 2

--- PalindromString.java
+++ PalindromString.java
@@ -2,7 +2,7 @@ package palindrome.string;
 
 import java.util.Scanner;
 
-public class PalindromString123 {
+public class PalindromString12345 {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
